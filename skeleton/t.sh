55b1d2c9b5057760db429645ee065b4c17ab41f3
thomaskim <kimthomas109@gmail.com>
2019-01-10 13:48:39 -0800

    Add hello world test and more tooling around running tests

--- skeleton/t.sh
+++ skeleton/t.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -e
 
 ant
-x=$1
-pushd scanner > /dev/null;
-  echo "Running test on ${x}..."
-  CMD="java -jar ../dist/Compiler.jar -target scan ${x}"
-  echo $CMD;
-  $CMD | tee /tmp/${x}.out;
-  diff output/${x}.out /tmp/${x}.out
+x=$2
+if [[ $1 == "scanner" ]]; then
+  pushd scanner > /dev/null;
+    echo "Running test on ${x}..."
+    CMD="java -jar ../dist/Compiler.jar -target scan ${x}"
+    echo $CMD;
+    $CMD | tee /tmp/${x}.out;
+    diff output/${x}.out /tmp/${x}.out
+  popd > /dev/null;
+elif [[ $1 == "parser" ]]; then
+  pushd parser > /dev/null;
+    echo "Running test on ${x}..."
+    CMD="java -jar ../dist/Compiler.jar -target parse ${x}"
+    echo $CMD;
+    $CMD;
 popd > /dev/null;
+fi
 
 
--- skeleton/test.sh
+++ skeleton/test.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -e
 
 ant
-pushd scanner > /dev/null;
-  for x in *; do
-    if [[ "${x}" != "output" ]]; then
+
+if [[ $1 == "scanner" ]]; then
+  pushd scanner > /dev/null;
+    for x in *; do
+      if [[ "${x}" != "output" ]]; then
+        echo "Running test on ${x}..."
+        CMD="java -jar ../dist/Compiler.jar -target scan ${x}"
+        echo $CMD;
+        $CMD | tee /tmp/${x}.out;
+        diff output/${x}.out /tmp/${x}.out
+      fi
+    done
+  popd > /dev/null;
+elif [[ $1 == "parser" ]]; then
+  pushd parser > /dev/null;
+    for x in *; do
       echo "Running test on ${x}..."
-      CMD="java -jar ../dist/Compiler.jar -target scan ${x}"
+      CMD="java -jar ../dist/Compiler.jar -target parse ${x}"
       echo $CMD;
-      $CMD | tee /tmp/${x}.out;
-      diff output/${x}.out /tmp/${x}.out
-    fi
-  done
-popd > /dev/null;
+      $CMD;
+    done
+  popd > /dev/null;
+fi
 
