872c50eb7693dcae6f406f459a43e0279ddf0d39
Andrew Schamp <schamp@dropbox.com>
2019-02-02 14:43:39 -0800

    convert a bunch of tests to new error styles

--- skeleton/test.sh
+++ skeleton/test.sh
@@ -1,16 +1,26 @@
 #!/bin/bash -e
 
+function run_test {
+  x=$1
+  echo "Running scanner test on ${x}..."
+  cat -n ${x}
+  CMD="java -jar ../dist/Compiler.jar -target scan ${x}"
+  echo $CMD;
+  $CMD 2>&1 | tee /tmp/${x}.out;
+  diff output/${x}.out /tmp/${x}.out
+}
+
 ant
 pushd scanner > /dev/null;
-  for x in *; do
-    if [[ "${x}" != "output" ]]; then
-      echo "Running test on ${x}..."
-      cat ${x}
-      CMD="java -jar ../dist/Compiler.jar -target scan ${x}"
-      echo $CMD;
-      $CMD | tee /tmp/${x}.out;
-      diff output/${x}.out /tmp/${x}.out
-    fi
-  done
+  if [[ "${1}" != "" ]]; then
+    run_test "$1"
+  else
+    for x in *; do
+      if [[ "${x}" != "output" ]]; then
+        run_test "${x}"
+        echo
+      fi
+    done
+  fi
 popd > /dev/null;
 
